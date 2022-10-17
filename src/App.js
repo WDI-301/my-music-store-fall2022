b9f2e502cd0513356f658854e595a076b1f4dc50
Omar Suriel <omarsuriel@icloud.com>
2022-10-17 13:51:49 -0400

    create-product-page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CartPage from './components/pages/CartPage';
+import CreateProductPage from './components/pages/CreateProductPage';
 import HomePage from './components/pages/HomePage';
 import SignInPage from './components/pages/SignInPage';
 import UserRegistrationPage from './components/pages/UserRegistrationPage';
@@ -20,6 +21,7 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/user" element={<SignInPage />} />
               <Route path="/register-user" element={<UserRegistrationPage />} />
+              <Route path="/create-product" element={<CreateProductPage />} />
               <Route
                 path="/cart"
                 element={(
---
+++ src/components/pages/CreateProductPage.js
@@ -0,0 +1,106 @@
+import { Box, Button, TextField } from '@mui/material';
+import { useState } from 'react';
+import Axios from '../../utils/Axios';
+import Layout from '../layout/Layout';
+
+function CreateProductPage() {
+  const [productData, setProductData] = useState({
+    title: '',
+    description: '',
+    brand: '',
+    price: '',
+    image: '',
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await Axios.post('/create-product', {
+      productData: {
+        title: productData.title,
+        description: productData.description,
+        brand: productData.brand,
+        price: Number(productData.price),
+        image: productData.image,
+      },
+    });
+
+    console.log('response: ', response);
+  };
+
+  return (
+    <Layout>
+      <form action="submit" onSubmit={handleSubmit}>
+        <Box mb={4}>
+          <TextField
+            id="title"
+            label="title"
+            value={productData.title}
+            onChange={(event) => setProductData({
+              ...productData,
+              title: event.target.value,
+            })}
+            required
+          />
+        </Box>
+        <Box mb={4}>
+          <TextField
+            id="description"
+            label="description"
+            value={productData.description}
+            onChange={(event) => setProductData({
+              ...productData,
+              description: event.target.value,
+            })}
+            required
+          />
+        </Box>
+        <Box mb={4}>
+          <TextField
+            id="brand"
+            label="brand"
+            value={productData.brand}
+            onChange={(event) => setProductData({
+              ...productData,
+              brand: event.target.value,
+            })}
+            required
+          />
+        </Box>
+        <Box mb={4}>
+          <TextField
+            id="price"
+            label="price"
+            type="number"
+            value={productData.price}
+            onChange={(event) => setProductData({
+              ...productData,
+              price: event.target.value,
+            })}
+            required
+          />
+        </Box>
+        <Box mb={4}>
+          <TextField
+            id="image"
+            label="image"
+            value={productData.image}
+            onChange={(event) => setProductData({
+              ...productData,
+              image: event.target.value,
+            })}
+            required
+          />
+        </Box>
+        <Box>
+          <img style={{ width: '200px', height: '200px', objectFit: 'cover' }} src={productData.image} alt="product" />
+        </Box>
+        <Box>
+          <Button type="submit">Submit</Button>
+        </Box>
+      </form>
+    </Layout>
+  );
+}
+
+export default CreateProductPage;
